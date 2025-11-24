95b7716c9cef96afbf46460856ec0fbc858b3f76
Craftsman-Building-Craft <xx.diegoperez@gmail.com>
2025-11-24 00:40:28 -0400

    Create creadores.js

---
+++ assets/js/creadores.js
@@ -0,0 +1,37 @@
+const creadores = [
+  {
+    nombre: "Astronauta29",
+    imagen: "./assets/images/FotosdePerfil/astronauta29.jpg",
+    link: "https://youtube.com/@astronautaa29"
+  },
+  {
+    nombre: "LuirryPlus",
+    imagen: "./assets/images/FotosdePerfil/LuirryPlus.jpg",
+    link: "https://www.youtube.com/@LuirryPlus"
+  },
+  {
+    nombre: "SoyPeche",
+    imagen: "./assets/images/FotosdePerfil/soypeche.jpg",
+    link: "https://youtube.com/@soypeche"
+  },
+  {
+    nombre: "VicMaster",
+    imagen: "./assets/images/FotosdePerfil/vicmaster.jpg",
+    link: "https://youtube.com/@vicmaster_"
+  },
+  {
+    nombre: "The Crafting Gamer Z",
+    imagen: "./assets/images/FotosdePerfil/TheCraftingGamerZ.jpg",
+    link: "https://youtube.com/@thecraftinggamerz1137"
+  },
+  {
+    nombre: "Andres Games",
+    imagen: "./assets/images/FotosdePerfil/AndrésGames.jpg",
+    link: "https://youtube.com/@andresuly"
+  },
+  {
+    nombre: "Jahir YT",
+    imagen: "./assets/images/FotosdePerfil/JahirYT.jpg",
+    link: "https://youtube.com/@jahiryt"
+  }
+];
